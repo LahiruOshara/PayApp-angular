86f63383b58febbbb23bc29c59927d12717c27e6
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2018-10-11 16:58:22 +0530

    git push

---
+++ src/app/routing/app-routing/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from '../../login/login.component';
+import {RegisterComponent} from '../../register/register.component';
+
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
