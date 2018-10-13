08b8ff49d20c3f64482ab01e7b6daf87bdaca62c
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2018-10-13 20:51:35 +0530

    git commit -m "routes resolved"

--- src/app/routing/app-routing/app-routing.module.ts
+++ src/app/routing/app-routing/app-routing.module.ts
@@ -8,7 +8,8 @@ import {HomeComponent} from '../../home/home.component';
 const routes: Routes = [
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent },
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
